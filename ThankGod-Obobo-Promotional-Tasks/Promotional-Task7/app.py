40e31e352bee32dfbb4167e6e5f38e40b5afe0a2
actokuyt <chibthankgod@gamil.com>
2024-08-05 11:38:38 +0100

    promotional task 6

--- ThankGod-Obobo-Promotional-Tasks/Promotional-Task7/app.py
+++ ThankGod-Obobo-Promotional-Tasks/Promotional-Task7/app.py
@@ -1,15 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Hello, Welcome to KodeCamp DevOps Bookcamp!"
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
-
 import os
 from flask import Flask
 
